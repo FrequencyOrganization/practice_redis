85efb42b968acb54a52fd0ded5caf3924eb2f348
greapflute <greapflutea2@yahoo.co.jp>
2016-01-16 23:14:36 +0900

    redisサーバ起動まで記述

--- Berksfile
+++
@@ -1,1 +0,0 @@
-source "https://api.berkshelf.com"
--- site-cookbooks/redis/recipes/default.rb
+++ site-cookbooks/redis/recipes/default.rb
@@ -1,7 +1,8 @@
 #設定値
-package_name = "redis-#{redis_version}"
-package_path = "#{Chef::Config[:file_cache_path]}/#{package_name}.tar.gz"
-source_url = "http://download.redis.io/releases/redis-#{redis_version}.tar.gz"
+package_name = "redis-#{node["redis"]["version"]}"
+download_directory = "#{Chef::Config[:file_cache_path]}"
+package_path = "#{download_directory}/#{package_name}.tar.gz"
+source_url = "http://download.redis.io/releases/#{package_name}.tar.gz"
 
 #Download
 remote_file package_path do
@@ -11,13 +12,26 @@ end
 
 bash "make redis and replace" do
   user 'vagrant'
-  password 'vagrant'
+  group 'vagrant'
   cwd '/home/vagrant'
-  environment '/home/vagrant'
+  environment "HOME" => '/home/vagrant'
   code <<-EOC
-    cd #{package_path}
-    tar xzf redis-#{redis_version}.tar.gz
-    cd redis-#{redis_version}
-    make
+    cd #{download_directory}
+    sudo tar xzf #{package_name}.tar.gz
+    cd #{package_name}
+    sudo make test
+    sudo make install
   EOC
 end
+
+# サービスを起動する
+bash "start_redis_server" do
+  user 'vagrant'
+  group 'vagrant'
+  cwd '/home/vagrant'
+  code <<-EOH
+    /usr/local/bin/redis-server &
+  EOH
+  not_if "ps aux | grep \[r\]edis-server"
+end
+
